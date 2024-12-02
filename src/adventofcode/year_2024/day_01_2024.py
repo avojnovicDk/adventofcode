af74ce5d638e7d333fa6c3cb8f181726eb32c3f7
Ana Vojnovic <ana.vojnovic@rabobank.nl>
2024-12-02 20:17:37 +0100

    refactor: type hints and rename

--- src/adventofcode/year_2024/day_01_2024.py
+++ src/adventofcode/year_2024/day_01_2024.py
@@ -5,7 +5,7 @@ from adventofcode.registry.decorators import register_solution
 from adventofcode.util.input_helpers import get_input_file_path, yield_lines
 
 
-def _split_lines_into_cols_of_ints(lines: Iterable[str]):
+def _split_into_cols(lines: Iterable[str]) -> tuple[list[int]]:
     col1, col2 = list(), list()
 
     for line in lines:
@@ -18,14 +18,14 @@ def _split_lines_into_cols_of_ints(lines: Iterable[str]):
 
 @register_solution(2024, 1, 1)
 def part_one(input_file_path: str) -> int:
-    col1, col2 = _split_lines_into_cols_of_ints(yield_lines(input_file_path))
+    col1, col2 = _split_into_cols(yield_lines(input_file_path))
 
     return sum(abs(b - a) for a, b in zip(sorted(col1), sorted(col2)))
 
 
 @register_solution(2024, 1, 2)
 def part_two(input_file_path: str) -> int:
-    col1, col2 = _split_lines_into_cols_of_ints(yield_lines(input_file_path))
+    col1, col2 = _split_into_cols(yield_lines(input_file_path))
     col1, col2 = Counter(col1), Counter(col2)
     
     return sum(n * count * col2[n] for n, count in col1.items())
