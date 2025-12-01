47a7f5a65402793b9167090c78d7f64fa392e587
Ana Vojnovic <ana.vojnovic@rabobank.nl>
2025-12-01 18:53:38 +0100

    Improvements

--- src/adventofcode/year_2025/day_01_2025.py
+++ src/adventofcode/year_2025/day_01_2025.py
@@ -9,24 +9,19 @@ def _yield_changes(input_file_path: str):
 
 @register_solution(2025, 1, 1)
 def part_one(input_file_path: str):
-    pos = 50
-    return sum(
-        (pos := pos + change) % 100 == 0
-        for change in _yield_changes(input_file_path)
-    )
+    pos, changes = 50, _yield_changes(input_file_path)
+    return sum((pos := pos + change) % 100 == 0 for change in changes)
 
 
 @register_solution(2025, 1, 2)
 def part_two(input_file_path: str):
-    pos, counter = 50, 0
-    for change in _yield_changes(input_file_path):
-        pos += change
-        if pos <= 0 and pos != change:
+    pos, counter, changes = 50, 0, _yield_changes(input_file_path)
+    for change in changes:
+        if (pos := pos + change) <= 0 and pos != change:
             counter += 1
         clicks, abs_pos = divmod(abs(pos), 100)
         pos = 100 - abs_pos if pos * abs_pos < 0 else abs_pos
         counter += clicks
-
     return counter
 
 
