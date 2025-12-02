5003d8e7167db1147b62594b6155752ca5bc9626
Ana Vojnovic <ana.vojnovic@rabobank.nl>
2025-12-03 00:04:32 +0100

    Refactoring

--- src/adventofcode/year_2025/day_02_2025.py
+++ src/adventofcode/year_2025/day_02_2025.py
@@ -4,19 +4,25 @@ from adventofcode.registry.decorators import register_solution
 from adventofcode.util.input_helpers import get_input_file_path, read_file
 
 
-def _yield_invalid_ids(range, are_reps_limited):
+def _yield_invalid_ids(range, repeater):
     start, end = map(int, range.split("-"))
-    is_possible_seq = lambda seq: int(seq + seq) <= end
-    for seq in takewhile(is_possible_seq, map(str, count(1))):
-        candidate = seq * 2
-        if are_reps_limited and int(candidate) < start:
-            continue
+    for seq in takewhile(lambda seq: int(seq + seq) <= end, map(str, count(1))):
+         for invalid_id in repeater(seq):
+            if invalid_id >= start:
+                if invalid_id <= end:
+                    yield invalid_id
+                else:
+                     break
 
-        while int(candidate) < start:
-            candidate += seq
 
-        if int(candidate) <= end:
-            yield int(candidate)
+def _yield_repeated(seq):
+    yield int(seq + seq)
+
+
+def _yield_repeated_unlimited(seq):
+    repeated = seq
+    while True:
+        yield int(repeated := repeated + seq)
 
 
 @register_solution(2025, 2, 1)
@@ -24,7 +30,7 @@ def part_one(input_file_path: str):
     return sum(
         invalid_id
         for range in read_file(input_file_path).split(",")
-        for invalid_id in _yield_invalid_ids(range, True)
+        for invalid_id in _yield_invalid_ids(range, _yield_repeated)
     )
 
 
@@ -33,7 +39,7 @@ def part_two(input_file_path: str):
     return sum(
         invalid_id
         for range in read_file(input_file_path).split(",")
-        for invalid_id in set(_yield_invalid_ids(range, False))
+        for invalid_id in set(_yield_invalid_ids(range, _yield_repeated_unlimited))
     )
 
 
