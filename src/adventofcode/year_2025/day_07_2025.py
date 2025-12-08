019f4c8d2ee6bf9f10c0e2fab1c1156b35f0aee6
Ana Vojnovic <ana.vojnovic@rabobank.nl>
2025-12-08 13:56:39 +0100

    Part 1

--- src/adventofcode/year_2025/day_07_2025.py
+++ src/adventofcode/year_2025/day_07_2025.py
@@ -1,47 +1,38 @@
 from adventofcode.registry.decorators import register_solution
-from adventofcode.util.exceptions import SolutionNotFoundError
 from adventofcode.util.input_helpers import get_input_file_path
 from adventofcode.util.input_helpers import yield_lines
-
 
 
 @register_solution(2025, 7, 1)
 def part_one(input_file_path: str):
     splitters = set()
-    row, col = 0, 0
-    start = (0,0) # (row, col)
-    for line in yield_lines(input_file_path):
-        if (col_ := line.find("S")) != -1:
-            start = (row, col_)
-        else:
-            for col_, char in enumerate(line):
-                if char == "^":
-                    splitters.add((row, col_))
-        row += 1
-    col = len(line.strip())
+    for row, line in enumerate(yield_lines(input_file_path)):
+        for col, char in enumerate(line.strip()):
+            if char == '^':
+                splitters.add((row, col))
+            elif char == 'S':
+                start = (row, col)
 
-    visited = set()
-    def calc_splitters_hit(start: tuple):
-        if start in visited:
+    def calc_splitters_hit(pos, row, col, visited):
+        if pos in visited:
             return 0
-        pos = start
         visited.add(pos)
-        while True:
-            new_pos = (pos[0]+1, pos[1])
-            if new_pos in visited:
-                return 0
-            if new_pos[0] > row-1 or new_pos[1] < 0 or new_pos[1] > col-1:
-                return 0
-            elif new_pos in splitters:
-                s = 1
-                s += calc_splitters_hit((new_pos[0], new_pos[1]-1))
-                s += calc_splitters_hit((new_pos[0], new_pos[1]+1))
-                return s
-            else:
-                pos = new_pos
-                visited.add(pos)
 
-    return calc_splitters_hit(start)
+        x, y = pos
+        while True:
+            x += 1
+            if (x, y) in visited or x >= row or y < 0 or y >= col:
+                return 0
+            elif (x, y) in splitters:
+                return (
+                    1
+                    + calc_splitters_hit((x, y - 1), row, col, visited)
+                    + calc_splitters_hit((x, y + 1), row, col, visited)
+                )
+            else:
+                visited.add((x, y))
+
+    return calc_splitters_hit(start, row, col, set())
 
 
 @register_solution(2025, 7, 2)
